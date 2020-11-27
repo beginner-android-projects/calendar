e3ed646c7bfe82eaef16b1f7edbdfe605b9526ea
kym1924 <kym1924@naver.com>
2020-11-28 00:25:48 +0900

    Add BindingAdapter setAdapter

---
+++ Calendar/app/src/main/java/com/example/calendar/CalendarAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.calendar
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.calendar.databinding.ItemDayBinding
+
+class CalendarAdapter(private var list: List<CalendarData>) : RecyclerView.Adapter<CalendarAdapter.VHolder>(){
+
+    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int)
+            = VHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_day, parent, false))
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: VHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    internal fun setData(list : List<CalendarData>) {
+        this.list = list
+        notifyDataSetChanged()
+    }
+
+    inner class VHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        private val binding : ItemDayBinding = DataBindingUtil.bind(itemView)!!
+
+        fun bind(calendarData : CalendarData) {
+            binding.setVariable(BR.calendarData, calendarData)
+        }
+    }
+}
--- Calendar/app/src/main/java/com/example/calendar/CalendarBinding.kt
+++ Calendar/app/src/main/java/com/example/calendar/CalendarBinding.kt
@@ -2,11 +2,21 @@ package com.example.calendar
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 
 object CalendarBinding {
     @BindingAdapter("nowYear", "nowMonth")
     @JvmStatic
     fun nowYearMonth(textView : TextView, year : Int, month : Int) {
         textView.text = "$year / $month"
+    }
+
+    @BindingAdapter("setAdapter")
+    @JvmStatic
+    fun setAdapter(recyclerView : RecyclerView, list : List<CalendarData>?) {
+        list?.let{
+            val adapter = CalendarAdapter(list)
+            recyclerView.adapter = adapter
+        }
     }
 }
