85d62f43733c0518663c9e20d7c49df024d407c2
kym1924 <kym1924@naver.com>
2020-11-28 01:00:43 +0900

    Add Observer in MainActivity

--- Calendar/app/src/main/java/com/example/calendar/MainActivity.kt
+++ Calendar/app/src/main/java/com/example/calendar/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.calendar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.calendar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,16 @@ class MainActivity : AppCompatActivity() {
         val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.calendarViewModel = calendarViewModel
         binding.lifecycleOwner = this
+    }
+
+    override fun onStart() {
+        super.onStart()
+        calendarViewModel.textMonth.observe(this, Observer { month ->
+            month?.let{calendarViewModel.getDayList()}
+        })
+
+        calendarViewModel.textYear.observe(this, Observer { year ->
+            year?.let{calendarViewModel.getDayList()}
+        })
     }
 }
